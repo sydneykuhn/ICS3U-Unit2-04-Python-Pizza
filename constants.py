c28304d5dbc1e1050af6c49744a45b55d53494da
Sydney <sydney.kuhn@mths.ca>
2021-09-14 22:26:40 +0000

    added price of a pizza.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Sep 2020
+# This file contains the constants for circle.py
+
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
---
+++ pizza.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Sep 2020
+# This program calculates the price of a pizza
+#    using the diameter provided by user
+
+import constants
+
+
+def main():
+
+    # this function calculates price
+
+    # input
+    diameter = int(input("Enter diameter of the Pizza (inches): "))
+
+    # process
+    sub_total = (
+        constants.LABOR + constants.RENT + (constants.COST_PER_INCH * diameter)
+    )
+    total = sub_total + (sub_total * constants.HST)
+
+    # output
+    print("")
+    print("The cost for a {0} inch pizza is ${1:,.2f}".format(diameter, total))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
